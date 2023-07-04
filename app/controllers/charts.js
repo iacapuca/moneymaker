4183f5fdacd183bafb54195e2b1f9e6e9fe5923d
Pedro Paes <plpbs@poli.br>
2023-07-03 23:55:00 -0300

    Using chartjs

---
+++ app/controllers/charts.js
@@ -0,0 +1,32 @@
+import Controller from '@ember/controller'
+import { tracked } from '@glimmer/tracking'
+
+export default class ChartsController extends Controller {
+  DATA_COUNT = 12
+  labels = []
+
+  datapoints = [0, 20, 20, 60, 60, 120, NaN, 180, 120, 125, 105, 110, 170]
+  data = {
+    labels: this.labels,
+    datasets: [
+      {
+        label: 'Cubic interpolation (monotone)',
+        data: this.datapoints,
+        fill: false,
+        cubicInterpolationMode: 'monotone',
+        tension: 0.4
+      },
+      {
+        label: 'Cubic interpolation',
+        data: this.datapoints,
+        fill: false,
+        tension: 0.4
+      },
+      {
+        label: 'Linear interpolation (default)',
+        data: this.datapoints,
+        fill: false
+      }
+    ]
+  }
+}
